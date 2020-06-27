32a9e3ca23cdafa05d11846105ccd9999fdf8bb7
abzaver <abzaver@gmail.com>
2020-06-27 08:24:48 +0300

    worked config

---
+++ esp8266-homie-temp-sensor.ino
@@ -0,0 +1,69 @@
+#define PIN_SIGNAL 15 //pin where locate Signal LED
+#define PIN_OWIRE  13 //pin where locate temperature sensor
+#define PIN_RESET  02 //reset pin
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+OneWire oneWire(PIN_OWIRE);
+DallasTemperature sensors(&oneWire);
+
+#include <Homie.h>
+
+const int TEMPERATURE_INTERVAL = 30;
+
+unsigned long lastTemperatureSent = 0;
+unsigned long lastSupplySent = 0;
+
+HomieNode temperatureNode("temperature", "Temperature", "temperature");
+HomieNode supplyNode("supply", "PowerSupply", "power-supply");
+
+
+void loopHandler() {
+  if (millis() - lastTemperatureSent >= TEMPERATURE_INTERVAL * 1000UL || lastTemperatureSent == 0) {
+    sensors.requestTemperatures(); // получаем значение температуры от датчика
+    float temperature = sensors.getTempCByIndex(0);
+    Homie.getLogger() << "Temperature: " << temperature << " °C" << endl;
+    temperatureNode.setProperty("degrees").send(String(temperature));
+    lastTemperatureSent = millis();
+  }
+  if (millis() - lastSupplySent >= TEMPERATURE_INTERVAL * 1000UL || lastSupplySent == 0) {
+    float volts = (analogRead(A0)/1023.0)*5; // Volts from ADC
+    Homie.getLogger() << "Supply voltage: " << volts << " V" << endl;
+    supplyNode.setProperty("supply-voltage").send(String(volts));
+
+    bool value = (volts > 3);
+    supplyNode.setProperty("supply-present").send(value ? "true" : "false");
+    Homie.getLogger() << "Supply present: " << (value ? "on" : "off") << endl;
+        
+    lastSupplySent = millis();
+  }  
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial << endl << endl;
+  Homie_setFirmware("temperature-sensor", "1.0.0");
+  Homie.setLoopFunction(loopHandler);
+  Homie.setLedPin(PIN_SIGNAL, HIGH);
+  
+  temperatureNode.advertise("degrees").setName("Degrees")
+                                      .setDatatype("float")
+                                      .setUnit("ºC");
+  
+  pinMode     (A0,         INPUT); // Initialize ADC for voltage measurement
+  supplyNode.advertise("supply-voltage").setName("Volts")
+                                      .setDatatype("float")
+                                      .setUnit("V");
+  
+  supplyNode.advertise("supply-present").setName("SupplyPresent")
+                                      .setDatatype("boolean");
+  
+  Homie.setResetTrigger(PIN_RESET, LOW, 2000); // before Homie.setup()
+  Homie.setup();
+  //Homie.reset();
+  randomSeed(micros());
+}
+
+void loop() {
+  Homie.loop();
+}
